5de3b68906bba496d6a27d7cf5b4458a3917b74d
Mohammed Natour <bgd4500@gmail.com>
2023-08-01 13:36:39 +0300

    email and password validate on the fly in signin page

--- app/src/main/java/com/example/logintask1/SigninActivity.kt
+++ app/src/main/java/com/example/logintask1/SigninActivity.kt
@@ -3,7 +3,9 @@ package com.example.logintask1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.text.SpannableString
+import android.text.TextWatcher
 import android.text.style.UnderlineSpan
 import android.util.Log
 import com.example.logintask1.databinding.ActivitySigninBinding
@@ -19,24 +21,19 @@ class SigninActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val createAccountText = binding.signinText
-        val emailAddressEditText = binding.emailEditText
-        val passwordEditText = binding.passwordEditText
         val signinButton = binding.signinButton
-        val emailContainer = binding.emailContainer
-        val passwordContainer = binding.passwordContainer
+
 
         val text = createAccountText.text
         val spannable = SpannableString(text)
         spannable.setSpan(UnderlineSpan(), 0, text.length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
         createAccountText.text = spannable
+        emailFocusListener()
+        passwordFocusListener()
 
         signinButton.setOnClickListener {
-
-            val password = passwordEditText.text.toString()
-            val emailAddress = emailAddressEditText.text.toString()
-            Log.d("validating", "validating email address $emailAddress")
-            val isEmailValid = validateEmail(emailAddress, emailContainer)
-            val isPasswordValid = validatePassword(password, passwordContainer)
+            val isPasswordValid = (binding.emailContainer.helperText == null)
+            val isEmailValid = (binding.passwordContainer.helperText == null)
             if(isEmailValid && isPasswordValid){
                 val mainPageIntent = Intent(this, MainActivity::class.java)
                 startActivity(mainPageIntent)
@@ -50,28 +47,61 @@ class SigninActivity : AppCompatActivity() {
 
     }
 
-    private fun validateEmail(email: String, emailContainer: TextInputLayout): Boolean{
-        val emailRegex = Regex(".+@.+[.com]$")
-        if(!emailRegex.matches(email)){
-            Log.d("match", "$email matches the pattern!")
-            emailContainer.helperText = "incorrect email pattern"
-            return false
-        }else{
-            emailContainer.helperText = ""
+    private fun emailFocusListener(){
+        val container = binding.emailContainer
+        binding.emailEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // do nothing
+            }
 
-        }
-        return true
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // do nothing
+            }
+
+            override fun afterTextChanged(newText: Editable?) {
+                container.helperText = validateEmail()
+            }
+        })
+
     }
 
-    private fun validatePassword(password: String, passwordContainer: TextInputLayout): Boolean{
-        if(password == ""){
-            Log.d("password", "please provide a password")
-            passwordContainer.helperText = "please provide a password"
-            return false
-        }else{
-            passwordContainer.helperText = ""
+    private fun passwordFocusListener(){
+        val container = binding.passwordContainer
+        binding.passwordEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // do nothing
+            }
+
+            override fun afterTextChanged(newText: Editable?) {
+                container.helperText = validatePassword()
+            }
+        })
+
+
+    }
+
+    private fun validateEmail(): String?{
+
+        val emailRegex = Regex(".+@.+[.com]$")
+        val email = binding.emailEditText.text.toString()
+
+        if(!emailRegex.matches(email)){
+            Log.d("match", "$email matches the pattern!")
+            return "incorrect email pattern"
         }
-        return true
+        return null
+    }
+
+    private fun validatePassword(): String?{
+        val password = binding.passwordEditText.text.toString()
+        if(password.length < 8){
+            return "password must be at least 8 characters"
+        }
+        return null
 
     }
 }
