6f1fe8d60bd14f3cca63dd9d236cedd2046a86c1
Mohammed Natour <bgd4500@gmail.com>
2023-08-08 17:26:41 +0300

    created signin viewModel file

--- app/src/main/java/com/example/logintask1/MainActivity.kt
+++ app/src/main/java/com/example/logintask1/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
--- app/src/main/java/com/example/logintask1/SigninFragment.kt
+++ app/src/main/java/com/example/logintask1/SigninFragment.kt
@@ -1,50 +1,39 @@
 package com.example.logintask1
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
-import android.text.SpannableString
 import android.text.TextWatcher
-import android.text.style.UnderlineSpan
 import android.util.Log
+import android.util.Log.ASSERT
+import android.util.Log.INFO
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.example.logintask1.databinding.FragmentSigninBinding
+import java.util.logging.Level.INFO
 
 
 class SigninFragment : Fragment(R.layout.fragment_signin) {
     private lateinit var binding: FragmentSigninBinding
+    private val model: SigninViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentSigninBinding.inflate(layoutInflater)
+        binding = FragmentSigninBinding.bind(view)
+        binding.viewModel = model
 
-        val createAccountText = binding.signinText
-        val signinButton = binding.signinButton
-
-
-        val text = createAccountText.text
-        val spannable = SpannableString(text)
-        spannable.setSpan(UnderlineSpan(), 0, text.length, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-        createAccountText.text = spannable
-        emailChangeListener()
-        passwordChangeListener()
-
-        signinButton.setOnClickListener {
-            val isPasswordValid = (binding.emailContainer.helperText == null)
-            val isEmailValid = (binding.passwordContainer.helperText == null)
-            if(isEmailValid && isPasswordValid){
-                Log.d("hi", "hi")
-            }
-        }
-        createAccountText.setOnClickListener {
-            Log.d("Debug", "creating new account")
+        val ob = Observer<String> { text ->
+            Log.d("textbox changed", text)
+            binding.emailContainer.helperText = text
 
         }
 
+        model.error.observe(viewLifecycleOwner, ob)
+        changeListener()
     }
 
-    private fun emailChangeListener(){
-        val container = binding.emailContainer
+    private fun changeListener(){
         binding.emailEditText.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 // do nothing
@@ -55,50 +44,10 @@ class SigninFragment : Fragment(R.layout.fragment_signin) {
             }
 
             override fun afterTextChanged(newText: Editable?) {
-                container.helperText = validateEmail()
+                model.updateError(newText.toString())
             }
         })
-
     }
 
-    private fun passwordChangeListener(){
-        val container = binding.passwordContainer
-        binding.passwordEditText.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                // do nothing
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                // do nothing
-            }
-
-            override fun afterTextChanged(newText: Editable?) {
-                container.helperText = validatePassword()
-            }
-        })
-
-
-    }
-
-    private fun validateEmail(): String?{
-
-        val emailRegex = Regex(".+@.+[.com]$")
-        val email = binding.emailEditText.text.toString()
-
-        if(!emailRegex.matches(email)){
-            Log.d("match", "$email matches the pattern!")
-            return "incorrect email pattern"
-        }
-        return null
-    }
-
-    private fun validatePassword(): String?{
-        val password = binding.passwordEditText.text.toString()
-        if(password.length < 8){
-            return "password must be at least 8 characters"
-        }
-        return null
-
-    }
 }
 
