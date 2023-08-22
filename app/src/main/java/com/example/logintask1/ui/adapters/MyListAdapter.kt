a46f190dbefe554e39d33dbe1c9356141e65552a
Mohammed Natour <bgd4500@gmail.com>
2023-08-22 15:09:36 +0300

    Revert "fix listadapter"
    This reverts commit 7c42bb782b67b3722d1dfbd3b6acd654cfadb483.

--- app/src/main/java/com/example/logintask1/ui/adapters/MyListAdapter.kt
+++ app/src/main/java/com/example/logintask1/ui/adapters/MyListAdapter.kt
@@ -2,12 +2,11 @@ package com.example.logintask1.ui.adapters
 
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.example.logintask1.data.ListItemClass
+import com.example.logintask1.data.listItemClass
 
-class myListAdapter: ListAdapter<ListItemClass, myListAdapter.MyViewHolder>(DiffCallback()) {
+class myListAdapter: ListAdapter<listItemClass, myListAdapter.MyViewHolder>() {
     class MyViewHolder(itemView: View) : ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -16,15 +15,5 @@ class myListAdapter: ListAdapter<ListItemClass, myListAdapter.MyViewHolder>(Diff
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         TODO("Not yet implemented")
-    }
-}
-
-class DiffCallback : DiffUtil.ItemCallback<ListItemClass>() {
-    override fun areItemsTheSame(oldItem: ListItemClass, newItem: ListItemClass): Boolean {
-        return oldItem == newItem
-    }
-
-    override fun areContentsTheSame(oldItem: ListItemClass, newItem: ListItemClass): Boolean {
-        return oldItem == newItem
     }
 }
