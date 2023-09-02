e23a18873930c2aa8eb2337faaf298a868360a8a
Mohammed Natour <bgd4500@gmail.com>
2023-09-02 23:46:10 +0300

    fix: fix fragment switching error

--- app/src/main/java/com/example/logintask1/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/logintask1/ui/auth/AuthActivity.kt
@@ -3,33 +3,40 @@ package com.example.logintask1
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.logintask1.databinding.ActivityAuthBinding
 import com.example.logintask1.ui.auth.signin.SigninFragmentDirections
+import com.example.logintask1.ui.auth.signup.SignupFragmentDirections
 import com.google.android.material.tabs.TabLayout
 
 
 class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        navController = findNavController(R.id.fragmentContainerView)
         binding.tabLayout.addOnTabSelectedListener(tabSelectListener)
     }
     private val tabSelectListener =  object: TabLayout.OnTabSelectedListener {
 
         override fun onTabSelected(tab: TabLayout.Tab?) {
             when (tab?.position) {
-                0 -> {
-                    val directions = SigninFragmentDirections.actionSigninFragmentToHomeActivity()
-//                    findNavController(R.id.).navigate(directions)
-                }
                 1 -> {
-                    val directions = SigninFragmentDirections.actionSigninFragmentToHomeActivity()
-//                    findNavController().navigate(directions)
+                    val action = SigninFragmentDirections.actionSigninFragmentToSignupFragment()
+//
+                    navController.navigate(action)
+//                    Log.d("pos", tab?.position.toString())
+                }
+                0 -> {
+                    val action = SignupFragmentDirections.actionSignupFragmentToSigninFragment()
+                    Log.d("pos", tab?.position.toString())
+                    navController.navigate(action)
                 }
             }
 
